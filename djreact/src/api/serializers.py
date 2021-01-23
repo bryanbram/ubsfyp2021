5ef13ac78bd9c771d6c0c65724244e3f653c5e16
10yilin <yilin.zhou.2018@sis.smu.edu.sg>
2021-01-23 15:24:08 +0800

    Update serializers.py

--- djreact/src/api/serializers.py
+++ djreact/src/api/serializers.py
@@ -1,6 +1,15 @@
 ### help to turn the content of a model into json format
 from rest_framework import serializers
 from .models import Account, Question, Option, Token, Chapter, Level, Mode,Game
+from djoser.serializers import UserCreateSerializer
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserCreateSerializer(UserCreateSerializer):
+    class Meta(UserCreateSerializer.Meta):
+        model = User
+        fields = ('email', 'username','password')
 
 class QuestionSerializer(serializers.ModelSerializer):
     serializers.ImageField(use_url=True, required=False, allow_null=True)
@@ -9,7 +18,7 @@ class QuestionSerializer(serializers.ModelSerializer):
         # fields = ('id', 'question_id', 'name', 'level','creation_time')
         fields = '__all__'
 
-class AccountSerializer(serializers.ModelSerializer):
+class AccountSerializer(UserCreateSerializer):
     class Meta:
         model = Account
         fields = ("username", "email", "password")#exclude = ('is_admin', 'is_active', 'last_login')
