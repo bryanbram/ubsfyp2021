f4bc5bd778a881a60f877211b0aef7626683f2b1
10yilin <yilin.zhou.2018@sis.smu.edu.sg>
2021-01-29 16:55:04 +0800

    change folder containers to `login`

--- djreact/src/frontend/src/App.js
+++ djreact/src/frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Home from './containers/Home';
-import Login from './containers/Login';
-import Signup from './containers/Signup';
-import Activate from './containers/Activate';
-import ResetPassword from './containers/ResetPassword';
-import ResetPasswordConfirm from './containers/ResetPasswordConfirm';
+import Home from './login/Home';
+import Login from './login/Login';
+import Signup from './login/Signup';
+import Activate from './login/Activate';
+import ResetPassword from './login/ResetPassword';
+import ResetPasswordConfirm from './login/ResetPasswordConfirm';
 
 
 import { Provider } from 'react-redux';
