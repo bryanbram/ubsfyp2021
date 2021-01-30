1ff51c2ff2db4eded1f6d0be5ad80a414d3adc2f
hilyamy2018 <54932191+hilyamy2018@users.noreply.github.com>
2021-01-30 22:27:23 +0800

    invite system

---
+++ djreact/src/friend/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class FriendConfig(AppConfig):
+    name = 'friend'
+
+# add 'friend' in setting.py
---
+++ djreact/src/friend/models.py
@@ -0,0 +1,90 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+
+
+class FriendList(models.Model):
+
+	user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="user")
+	friends = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name="friends") 
+
+	def __str__(self):
+		return self.user.username
+
+	def add_friend(self, account):
+		# Add a new friend [send invitation]
+		if not account in self.friends.all():
+			self.friends.add(account)
+			self.save()
+
+	def remove_friend(self, account):
+		# Remove a friend [remove invitation]
+		if account in self.friends.all():
+			self.friends.remove(account)
+
+	def unfriend(self, removee):
+		# Initiate the action of unfriending someone. []
+		remover_friends_list = self # person terminating the inviation
+
+		# Remove friend from remover friend list
+		remover_friends_list.remove_friend(removee)
+
+		# Remove friend from removee friend list
+		friends_list = FriendList.objects.get(user=removee)
+		friends_list.remove_friend(remover_friends_list.user)
+
+
+
+class FriendRequest(models.Model):
+	"""
+	A friend request consists of two main parts:
+		1. SENDER
+			- Person sending/initiating the invitation
+		2. RECIVER
+			- Person receiving the invitation
+	"""
+
+	sender 	= models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="sender")
+	receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="receiver")
+
+	is_active = models.BooleanField(blank=False, null=False, default=True)
+
+	timestamp = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.sender.username
+
+	def accept(self):
+		# Accept an invitation.
+		# will update both SENDER and RECEIVER's list in the team page.
+		receiver_friend_list = FriendList.objects.get(user=self.receiver)
+		if receiver_friend_list:
+			receiver_friend_list.add_friend(self.sender)
+			sender_friend_list = FriendList.objects.get(user=self.sender)
+			if sender_friend_list:
+				sender_friend_list.add_friend(self.receiver)
+				self.is_active = False
+				self.save()
+
+	def decline(self):
+		# Decline an invitation.
+		self.is_active = False
+		self.save()
+
+
+	def cancel(self):
+		# Cancel invitation that is already sent.
+		self.is_active = False
+		self.save()
+
+
+
+
+
+
+
+
+
+
+
